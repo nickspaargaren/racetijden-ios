f33557d6cf865e675f33c3d944883fdd9a6b7131
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2023-09-03 12:39:24 +0200

    Initial Commit

---
+++ Shared/racetijden_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  racetijden_iosApp.swift
+//  Shared
+//
+//  Created by Nick Spaargaren on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct racetijden_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
