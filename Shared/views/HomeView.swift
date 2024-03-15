18e09ea935033e914cedc343b06d10bf0a01ac71
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-03-15 20:43:59 +0100

    Add search in circuit overview

--- Shared/views/HomeView.swift
+++ Shared/views/HomeView.swift
@@ -3,17 +3,26 @@ import SwiftUI
 struct HomeView: View {
     
     @ObservedObject var api = Api()
+    @State private var searchQuery = ""
     
     var body: some View {
         NavigationView {
-            List(api.circuits) { circuit in
-                CircuitItemView(flag: circuit.flag, name: circuit.name, description: circuit.description, winner: !circuit.times.isEmpty ? circuit.times[0].gamertag : "")
-            }.navigationTitle("Circuits")
-                .onAppear {
-                    Task {
-                        await api.fetchCircuits()
-                    }
+            List {
+                ForEach(api.circuits.filter {
+                    searchQuery.isEmpty ||
+                    $0.name.localizedCaseInsensitiveContains(searchQuery) ||
+                    $0.description.localizedCaseInsensitiveContains(searchQuery)
+                }, id: \.id) { circuit in
+                    CircuitItemView(flag: circuit.flag, name: circuit.name, description: circuit.description, winner: !circuit.times.isEmpty ? circuit.times[0].gamertag : "")
                 }
+            }
+            .navigationTitle("Circuits")
+            .searchable(text: $searchQuery)
+            .onAppear {
+                Task {
+                    await api.fetchCircuits()
+                }
+            }
         }
     }
 }
