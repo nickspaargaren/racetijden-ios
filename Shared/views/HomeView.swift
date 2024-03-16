7fcd0a527f2a982b88c38f9c4e7d369f4a2e62a4
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-03-16 09:17:19 +0100

    Add loading state to circuits list

--- Shared/views/HomeView.swift
+++ Shared/views/HomeView.swift
@@ -4,24 +4,36 @@ struct HomeView: View {
     
     @ObservedObject var api = Api()
     @State private var searchQuery = ""
+    @State private var isLoading = false
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(api.circuits.filter {
-                    searchQuery.isEmpty ||
-                    $0.name.localizedCaseInsensitiveContains(searchQuery) ||
-                    $0.description.localizedCaseInsensitiveContains(searchQuery)
-                }, id: \.id) { circuit in
-                    CircuitItemView(flag: circuit.flag, name: circuit.name, description: circuit.description, winner: !circuit.times.isEmpty ? circuit.times[0].gamertag : "")
+            Group {
+                if isLoading {
+                    ProgressView("Loading...")
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .onAppear {
+                            Task {
+                                await api.fetchCircuits()
+                                isLoading = false
+                            }
+                        }
+                } else {
+                    List {
+                        ForEach(api.circuits.filter {
+                            searchQuery.isEmpty ||
+                            $0.name.localizedCaseInsensitiveContains(searchQuery) ||
+                            $0.description.localizedCaseInsensitiveContains(searchQuery)
+                        }, id: \.id) { circuit in
+                            CircuitItemView(flag: circuit.flag, name: circuit.name, description: circuit.description, winner: !circuit.times.isEmpty ? circuit.times[0].gamertag : "")
+                        }
+                    }
                 }
             }
             .navigationTitle("Circuits")
             .searchable(text: $searchQuery)
             .onAppear {
-                Task {
-                    await api.fetchCircuits()
-                }
+                isLoading = true
             }
         }
     }
